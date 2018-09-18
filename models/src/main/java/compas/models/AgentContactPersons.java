e79bc94714af40ae2479533cd0a6dfe1d49a863d
jonah <jonah.kimutai@compulynx.com>
2018-09-18 17:02:46 +0300

    #agent contact person model

---
+++ models/src/main/java/compas/models/AgentContactPersons.java
@@ -0,0 +1,101 @@
+package compas.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by CLLSDJACKT013 on 8/20/2018.
+ */
+@Entity
+public class AgentContactPersons {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer Id;
+    private String agent_code;
+    private String fullname;
+    private String id_number;
+    private String email_address;
+    private String postal_address;
+    private String postal_code;
+    private String town;
+    private String gender;
+
+    //default constructors
+    public AgentContactPersons(){}
+
+    //generate getters and setters here
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getAgent_code() {
+        return agent_code;
+    }
+
+    public void setAgent_code(String agent_code) {
+        this.agent_code = agent_code;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getId_number() {
+        return id_number;
+    }
+
+    public void setId_number(String id_number) {
+        this.id_number = id_number;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public String getPostal_address() {
+        return postal_address;
+    }
+
+    public void setPostal_address(String postal_address) {
+        this.postal_address = postal_address;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
